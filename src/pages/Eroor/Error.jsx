c4f595dd7b42440afd7c14e3dfa9deeedbee4c40
Tanjilul Islam <tanjilulislam37@gmail.com>
2025-11-30 10:52:19 +0600

    error page added

--- src/pages/Eroor/Error.jsx
+++ src/pages/Eroor/Error.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import errorImg from '../../assets/Eroor.png'
+import { Link } from 'react-router';
 
 const Error = () => {
     return (
         <div className='bg-white rounded-4xl py-[80px] text-center'>
-            error
+            <img className='mx-auto' src={errorImg} alt="" />
+
+                <Link to='/' className='btn bg-primary'>Go Home</Link>
         </div>
     );
 };
